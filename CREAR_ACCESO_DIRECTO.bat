b8cf99636e4c627f19293059d0b2ee6c9100e0bd
Deiby Escobar <escobardeiby21-cmyk@github.com>
2026-08-16 21:30:56 +0200

    Add automatic Desktop shortcut generator for Python Desktop CRM

---
+++ CREAR_ACCESO_DIRECTO.bat
@@ -0,0 +1,12 @@
+@echo off
+set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
+echo sLinkFile = oWS.SpecialFolders("Desktop") ^& "\QueChimbaParce_Pro_CRM.lnk" >> %SCRIPT%
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
+echo oLink.TargetPath = "%~dp0INICIAR_APP_ESCRITORIO_CRM.bat" >> %SCRIPT%
+echo oLink.WorkingDirectory = "%~dp0" >> %SCRIPT%
+echo oLink.Description = "App de Escritorio CRM Que Chimba Parce" >> %SCRIPT%
+echo oLink.Save >> %SCRIPT%
+cscript /nologo %SCRIPT%
+del %SCRIPT%
+echo ¡Acceso directo creado en tu Escritorio de Windows con éxito!
