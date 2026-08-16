b6e3b03dfaa1d343a4839acad771fe12dff15f2f
Deiby Escobar <escobardeiby21-cmyk@github.com>
2026-08-16 22:33:15 +0200

    Add master 1-click launcher ⚡_INICIAR_COMPLETO_PC_DUENO.bat for owner's PC deployment

--- CREAR_ACCESO_DIRECTO.bat
+++ CREAR_ACCESO_DIRECTO.bat
@@ -3,9 +3,9 @@ set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
 echo sLinkFile = oWS.SpecialFolders("Desktop") ^& "\QueChimbaParce_Pro_CRM.lnk" >> %SCRIPT%
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
-echo oLink.TargetPath = "%~dp0INICIAR_APP_ESCRITORIO_CRM.bat" >> %SCRIPT%
+echo oLink.TargetPath = "%~dp0⚡_INICIAR_COMPLETO_PC_DUENO.bat" >> %SCRIPT%
 echo oLink.WorkingDirectory = "%~dp0" >> %SCRIPT%
-echo oLink.Description = "App de Escritorio CRM Que Chimba Parce" >> %SCRIPT%
+echo oLink.Description = "Sistema Empresarial CRM & ERP Que Chimba Parce" >> %SCRIPT%
 echo oLink.Save >> %SCRIPT%
 cscript /nologo %SCRIPT%
 del %SCRIPT%
